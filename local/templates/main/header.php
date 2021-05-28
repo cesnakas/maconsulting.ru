b6fc65d9a36abb6bd096faaa468d1af85595bc87
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-05-29 01:05:59 +0400

    Add css & js

---
+++ local/templates/main/header.php
@@ -0,0 +1,45 @@
+<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true)die();
+use Bitrix\Main\Page\Asset;
+$asset = Asset::getInstance();
+?>
+<!DOCTYPE html>
+<html lang="<?=LANGUAGE_ID;?>">
+<head>
+
+<?php
+    $APPLICATION->ShowHead();
+    // Meta
+    $asset->addString('<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover, user-scalable=0">');
+    // JQuery
+    $asset->addJs('https://code.jquery.com/jquery-3.6.0.min.js');
+    // Bootstrap
+    $asset->addCss('https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css');
+    $asset->addJs('https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js');
+    // Fonts
+    $asset->addCss(SITE_TEMPLATE_PATH.'/dist/css/fontello.css');
+    // simpleParallax
+    $asset->addJs('https://cdn.jsdelivr.net/npm/simple-parallax-js@5.6.1/dist/simpleParallax.min.js');
+    // Swiper
+    $asset->addCss('https://unpkg.com/swiper/swiper-bundle.min.css');
+    $asset->addJs('https://unpkg.com/swiper/swiper-bundle.min.js');
+    // Animate CSS
+    $asset->addCss('https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css');
+    $asset->addJs('https://cdn.jsdelivr.net/npm/@cycjimmy/swiper-animation@4/dist/swiper-animation.umd.min.js');
+    // Custom
+    $asset->addCss(SITE_TEMPLATE_PATH.'/dist/css/main.css');
+    $asset->addCss(SITE_TEMPLATE_PATH.'/dist/css/media.css');
+    $asset->addJs(SITE_TEMPLATE_PATH.'/dist/js/common.js');
+?>
+
+    <title><?$APPLICATION->ShowTitle();?></title>
+
+    <link rel="shortcut icon" href="<?=SITE_TEMPLATE_PATH?>/dist/img/favicon.png" type="image/png">
+    <link rel="icon" href="<?=SITE_TEMPLATE_PATH?>/dist/img/favicon.png" type="image/png">
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+
+</head>
+<body>
+
+    <div id="panel"><?$APPLICATION->ShowPanel();?></div>
+
