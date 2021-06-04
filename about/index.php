29743227a9cefed9c3eff6d3c705bc529f4ce935
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-06-04 16:23:51 +0400

    Updated about

--- about/index.php
+++ about/index.php
@@ -132,50 +132,63 @@ $asset->addJs('https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.
                 <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
                     <h2 class="title animUp _anim-items _anim-no-hide">Наши принципы</h2>
                 </div>
-                <div class="col-12">
-                    <div class="spoilersWrap animUp _anim-items _anim-no-hide">
-                        <div class="spoilersItem">
-                            <button class="spoilersItem__btn active">Профессионализм<span class="spoilersItem__icon"></span></button>
-                            <div class="spoilersItem__content" style="display: block;">
-                                <div class="spoilersItem__flex">
-                                    <div class="spoilersItem__text">
-                                        <p>Глубокое знание рынка, профессиональное ориентирование в системе нормативно-правового регулирования,  непрерывный мониторинг изменений и умение прогнозировать тенденции в сфере здравоохранения помогают нам оказывать высококвалифицированные услуги, используя современные и актуальные подходы.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="spoilersItem">
-                            <button class="spoilersItem__btn">Клиентоориентированность<span class="spoilersItem__icon"></span></button>
-                            <div class="spoilersItem__content">
-                                <div class="spoilersItem__flex">
-                                    <div class="spoilersItem__text">
-                                        <p>Глубокое знание рынка, профессиональное ориентирование в системе нормативно-правового регулирования,  непрерывный мониторинг изменений и умение прогнозировать тенденции в сфере здравоохранения помогают нам оказывать высококвалифицированные услуги, используя современные и актуальные подходы.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="spoilersItem">
-                            <button class="spoilersItem__btn">Инновационность<span class="spoilersItem__icon"></span></button>
-                            <div class="spoilersItem__content">
-                                <div class="spoilersItem__flex">
-                                    <div class="spoilersItem__text">
-                                        <p>Глубокое знание рынка, профессиональное ориентирование в системе нормативно-правового регулирования,  непрерывный мониторинг изменений и умение прогнозировать тенденции в сфере здравоохранения помогают нам оказывать высококвалифицированные услуги, используя современные и актуальные подходы.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="spoilersItem">
-                            <button class="spoilersItem__btn">Юридические и этические нормы<span class="spoilersItem__icon"></span></button>
-                            <div class="spoilersItem__content">
-                                <div class="spoilersItem__flex">
-                                    <div class="spoilersItem__text">
-                                        <p>Глубокое знание рынка, профессиональное ориентирование в системе нормативно-правового регулирования,  непрерывный мониторинг изменений и умение прогнозировать тенденции в сфере здравоохранения помогают нам оказывать высококвалифицированные услуги, используя современные и актуальные подходы.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:news.list",
+                    "about__list",
+                    Array(
+                        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                        "ADD_SECTIONS_CHAIN" => "N",
+                        "AJAX_MODE" => "N",
+                        "AJAX_OPTION_ADDITIONAL" => "",
+                        "AJAX_OPTION_HISTORY" => "N",
+                        "AJAX_OPTION_JUMP" => "N",
+                        "AJAX_OPTION_STYLE" => "N",
+                        "CACHE_FILTER" => "N",
+                        "CACHE_GROUPS" => "Y",
+                        "CACHE_TIME" => "36000000",
+                        "CACHE_TYPE" => "A",
+                        "CHECK_DATES" => "Y",
+                        "DETAIL_URL" => "",
+                        "DISPLAY_BOTTOM_PAGER" => "N",
+                        "DISPLAY_DATE" => "N",
+                        "DISPLAY_NAME" => "Y",
+                        "DISPLAY_PICTURE" => "Y",
+                        "DISPLAY_PREVIEW_TEXT" => "Y",
+                        "DISPLAY_TOP_PAGER" => "N",
+                        "FIELD_CODE" => array("", ""),
+                        "FILTER_NAME" => "",
+                        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                        "IBLOCK_ID" => "4",
+                        "IBLOCK_TYPE" => "lists",
+                        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                        "INCLUDE_SUBSECTIONS" => "Y",
+                        "MESSAGE_404" => "",
+                        "NEWS_COUNT" => "20",
+                        "PAGER_BASE_LINK_ENABLE" => "N",
+                        "PAGER_DESC_NUMBERING" => "N",
+                        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                        "PAGER_SHOW_ALL" => "N",
+                        "PAGER_SHOW_ALWAYS" => "N",
+                        "PAGER_TEMPLATE" => ".default",
+                        "PAGER_TITLE" => "Новости",
+                        "PARENT_SECTION" => "",
+                        "PARENT_SECTION_CODE" => "",
+                        "PREVIEW_TRUNCATE_LEN" => "",
+                        "PROPERTY_CODE" => array("", ""),
+                        "SET_BROWSER_TITLE" => "N",
+                        "SET_LAST_MODIFIED" => "N",
+                        "SET_META_DESCRIPTION" => "N",
+                        "SET_META_KEYWORDS" => "N",
+                        "SET_STATUS_404" => "N",
+                        "SET_TITLE" => "N",
+                        "SHOW_404" => "N",
+                        "SORT_BY1" => "ID",
+                        "SORT_BY2" => "ID",
+                        "SORT_ORDER1" => "ASC",
+                        "SORT_ORDER2" => "ASC",
+                        "STRICT_SECTION_CHECK" => "N"
+                    )
+                );?>
             </div>
 
         </div>
@@ -196,13 +209,48 @@ $asset->addJs('https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.
             <div class="row">
                 <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6"></div>
                 <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-                    <h2 class="title animUp _anim-items _anim-no-hide">Направления работы в 2021 году</h2>
+                    <h2 class="title animUp _anim-items _anim-no-hide">
+                        <? // Направления работы в 2021 году
+                        $APPLICATION->IncludeComponent(
+                            "bitrix:main.include",
+                            "",
+                            Array(
+                                "AREA_FILE_SHOW" => "file",
+                                "AREA_FILE_SUFFIX" => "inc",
+                                "EDIT_TEMPLATE" => "",
+                                "PATH" => "/include/about__areaswork-title.php"
+                            )
+                        );?>
+                    </h2>
                 </div>
                 <div class="col-12">
-                    <h3 class="subtitle animUp _anim-items _anim-no-hide">Тренинги и образовательные программы</h3>
+                    <h3 class="subtitle animUp _anim-items _anim-no-hide">
+                        <? // Тренинги и образовательные программы
+                        $APPLICATION->IncludeComponent(
+                            "bitrix:main.include",
+                            "",
+                            Array(
+                                "AREA_FILE_SHOW" => "file",
+                                "AREA_FILE_SUFFIX" => "inc",
+                                "EDIT_TEMPLATE" => "",
+                                "PATH" => "/include/about__areaswork-subtitle.php"
+                            )
+                        );?>
+                    </h3>
                 </div>
                 <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-                    <div class="direction_of_work__caption animUp _anim-items _anim-no-hide">Текст про то, что мы выбрали наиболее актуальные для российского рынка вопросы, что мы следим за тенденциями и корректируем повестку……</div>
+                    <div class="direction_of_work__caption animUp _anim-items _anim-no-hide">
+                        <?$APPLICATION->IncludeComponent(
+                            "bitrix:main.include",
+                            "",
+                            Array(
+                                "AREA_FILE_SHOW" => "file",
+                                "AREA_FILE_SUFFIX" => "inc",
+                                "EDIT_TEMPLATE" => "",
+                                "PATH" => "/include/about__areaswork-left.php"
+                            )
+                        );?>
+                    </div>
                     <a href="<?=SITE_DIR?>projects/" class="d-none d-md-block more-link animUp _anim-items _anim-no-hide">Примеры наших<br>реализованных проектов</a>
                 </div>
                 <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
@@ -211,25 +259,73 @@ $asset->addJs('https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.
                             <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
                                 <div class="direction_of_work__item">
                                     <div class="direction_of_work__count">01</div>
-                                    <div class="direction_of_work__name">Генно-клеточная терапия и молекулярно-генетическая диагностика</div>
+                                    <div class="direction_of_work__name">
+                                        <? // Генно-клеточная терапия и молекулярно-генетическая диагностика
+                                        $APPLICATION->IncludeComponent(
+                                            "bitrix:main.include",
+                                            "",
+                                            Array(
+                                                "AREA_FILE_SHOW" => "file",
+                                                "AREA_FILE_SUFFIX" => "inc",
+                                                "EDIT_TEMPLATE" => "",
+                                                "PATH" => "/include/about__areaswork-right-01.php"
+                                            )
+                                        );?>
+                                    </div>
                                 </div>
                             </div>
                             <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
                                 <div class="direction_of_work__item">
                                     <div class="direction_of_work__count">02</div>
-                                    <div class="direction_of_work__name">Применение «офф-лейбл» лекарственных препаратов в клинической практике</div>
+                                    <div class="direction_of_work__name">
+                                        <? // Применение «офф-лейбл» лекарственных препаратов в клинической практике
+                                        $APPLICATION->IncludeComponent(
+                                            "bitrix:main.include",
+                                            "",
+                                            Array(
+                                                "AREA_FILE_SHOW" => "file",
+                                                "AREA_FILE_SUFFIX" => "inc",
+                                                "EDIT_TEMPLATE" => "",
+                                                "PATH" => "/include/about__areaswork-right-02.php"
+                                            )
+                                        );?>
+                                    </div>
                                 </div>
                             </div>
                             <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
                                 <div class="direction_of_work__item">
                                     <div class="direction_of_work__count">03</div>
-                                    <div class="direction_of_work__name">Инновационные модели лекарственного обеспечения. Инновационные контракты</div>
+                                    <div class="direction_of_work__name">
+                                        <? // Инновационные модели лекарственного обеспечения. Инновационные контракты
+                                        $APPLICATION->IncludeComponent(
+                                            "bitrix:main.include",
+                                            "",
+                                            Array(
+                                                "AREA_FILE_SHOW" => "file",
+                                                "AREA_FILE_SUFFIX" => "inc",
+                                                "EDIT_TEMPLATE" => "",
+                                                "PATH" => "/include/about__areaswork-right-03.php"
+                                            )
+                                        );?>
+                                    </div>
                                 </div>
                             </div>
                             <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
                                 <div class="direction_of_work__item">
                                     <div class="direction_of_work__count">04</div>
-                                    <div class="direction_of_work__name">Повышение доступности оказания медицинской помощи для пациентов с редкими (орфанными) заболеваниями, ССЗ, онкологическими заболеваниями, онко гематологическими заболеваниями</div>
+                                    <div class="direction_of_work__name">
+                                        <? // Повышение доступности оказания медицинской помощи для пациентов с редкими (орфанными) заболеваниями, ССЗ, онкологическими заболеваниями, онко гематологическими заболеваниями
+                                        $APPLICATION->IncludeComponent(
+                                            "bitrix:main.include",
+                                            "",
+                                            Array(
+                                                "AREA_FILE_SHOW" => "file",
+                                                "AREA_FILE_SUFFIX" => "inc",
+                                                "EDIT_TEMPLATE" => "",
+                                                "PATH" => "/include/about__areaswork-right-04.php"
+                                            )
+                                        );?>
+                                    </div>
                                 </div>
                             </div>
                             <div class="col-12 d-block d-md-none">
--- local/dev/index.php
+++ local/dev/index.php
@@ -3,76 +3,7 @@ require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle("");
 ?>
 
-    <div class="our-principles">
-        <div class="anchor" id="page2"></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6"></div>
-                <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-                    <h2 class="title animUp _anim-items _anim-no-hide">Наши принципы</h2>
-                </div>
 
-<?$APPLICATION->IncludeComponent(
-    "bitrix:news.list",
-    "about__list",
-    Array(
-        "ACTIVE_DATE_FORMAT" => "d.m.Y",
-        "ADD_SECTIONS_CHAIN" => "N",
-        "AJAX_MODE" => "N",
-        "AJAX_OPTION_ADDITIONAL" => "",
-        "AJAX_OPTION_HISTORY" => "N",
-        "AJAX_OPTION_JUMP" => "N",
-        "AJAX_OPTION_STYLE" => "N",
-        "CACHE_FILTER" => "N",
-        "CACHE_GROUPS" => "Y",
-        "CACHE_TIME" => "36000000",
-        "CACHE_TYPE" => "A",
-        "CHECK_DATES" => "Y",
-        "DETAIL_URL" => "",
-        "DISPLAY_BOTTOM_PAGER" => "N",
-        "DISPLAY_DATE" => "N",
-        "DISPLAY_NAME" => "Y",
-        "DISPLAY_PICTURE" => "Y",
-        "DISPLAY_PREVIEW_TEXT" => "Y",
-        "DISPLAY_TOP_PAGER" => "N",
-        "FIELD_CODE" => array("", ""),
-        "FILTER_NAME" => "",
-        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
-        "IBLOCK_ID" => "4",
-        "IBLOCK_TYPE" => "lists",
-        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-        "INCLUDE_SUBSECTIONS" => "Y",
-        "MESSAGE_404" => "",
-        "NEWS_COUNT" => "20",
-        "PAGER_BASE_LINK_ENABLE" => "N",
-        "PAGER_DESC_NUMBERING" => "N",
-        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-        "PAGER_SHOW_ALL" => "N",
-        "PAGER_SHOW_ALWAYS" => "N",
-        "PAGER_TEMPLATE" => ".default",
-        "PAGER_TITLE" => "Новости",
-        "PARENT_SECTION" => "",
-        "PARENT_SECTION_CODE" => "",
-        "PREVIEW_TRUNCATE_LEN" => "",
-        "PROPERTY_CODE" => array("", ""),
-        "SET_BROWSER_TITLE" => "N",
-        "SET_LAST_MODIFIED" => "N",
-        "SET_META_DESCRIPTION" => "N",
-        "SET_META_KEYWORDS" => "N",
-        "SET_STATUS_404" => "N",
-        "SET_TITLE" => "N",
-        "SHOW_404" => "N",
-        "SORT_BY1" => "ID",
-        "SORT_BY2" => "ID",
-        "SORT_ORDER1" => "ASC",
-        "SORT_ORDER2" => "ASC",
-        "STRICT_SECTION_CHECK" => "N"
-    )
-);?>
-
-            </div>
-        </div>
-    </div>
 
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
