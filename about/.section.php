f307be185218fb7b378b434fc68615e8a822ac23
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-09-24 22:16:24 +0400

    Update about

--- about/.section.php
+++ about/.section.php
@@ -1,7 +1,7 @@
-<?
+<?php
 $sSectionName = "О компании";
-$arDirProperties = Array(
+$arDirProperties = [
     "description" => "О компании — Market Access Consulting — агентство полного цикла",
     "keywords" => "О компании, Market Access Consulting, агентство полного цикла",
     "TITLE" => "О компании — Market Access Consulting"
-);
+];
